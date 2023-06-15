20010067e47d4fc7b2a2e6c51950f4a5722f106c
naoyahorai <0911horai@gmail.com>
2023-06-15 11:48:25 +0900

    Qtでppmを表示してみる。

---
+++ Qt_Test/CMakeFiles/Qt_Test2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Qt_Test2_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Qt_Test2_autogen.dir/ParseCache.txt"
+  "Qt_Test2_autogen"
+  "CMakeFiles/Qt_Test2.dir/Qt_Test2_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Qt_Test2.dir/Qt_Test2_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/Qt_Test2.dir/main.cpp.o"
+  "CMakeFiles/Qt_Test2.dir/main.cpp.o.d"
+  "Qt_Test2"
+  "Qt_Test2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Qt_Test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Qt_Test/main.cpp
+++ Qt_Test/main.cpp
@@ -1,9 +1,15 @@
 #include <QtWidgets/QApplication>
 #include <QLabel>
+#include <QPixmap>
 
 int main(int argc, char *argv[]){
     QApplication app(argc, argv);
     QLabel *label = new QLabel("Hello World!");
+    QPixmap *pix = new QPixmap("image.ppm");
+
+    label -> setPixmap(*pix);
     label -> show();
+    
+    
     return app.exec();
 }
